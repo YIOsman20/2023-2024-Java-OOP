d2bf52427116c3df8bc68cab7ad9f45109179589
YIOsman20 <YIOsman20@codingburgas.bg>
2023-10-10 09:35:30 +0300

    task 2 3 and 4

---
+++ project 1/src/zada4a3/zada4a3.java
@@ -0,0 +1,9 @@
+package zada4a3;
+
+public class zada4a3
+{
+    public static void main(String[] args)
+    {
+        System.out.println("1 2 3 4 5");
+    }
+}
---
+++ project 1/src/zada4a4/zada4a4.java
@@ -0,0 +1,11 @@
+package zada4a4;
+
+public class zada4a4
+{
+    public static void main(String[] args)
+    {
+        System.out.println("1 2 3 4 5");
+        System.out.println("1 2 3 4 5");
+        System.out.println("1 2 3 4 5");
+    }
+}
