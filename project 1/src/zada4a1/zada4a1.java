7f3a10c145736bdea78cf5b019a21c9009747302
Юсмен Исмаил Осман <YIOsman20@codingburgas.bg>
2023-10-10 09:26:02 +0300

    fix

--- project 1/src/zada4a1/zada4a1.java
+++ project 1/src/zada4a1/zada4a1.java
@@ -1,4 +1,6 @@
-public class Main {
+package zada4a1;
+
+public class zada4a1 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This is another line");
